4a9b2676eadd851f8981e90d26213393cea26c1c
tungnguyen28 <90412641+tungnguyen28@users.noreply.github.com>
2023-05-08 10:35:46 +0700

    logolist change

--- src/components/LogoList/index.jsx
+++ src/components/LogoList/index.jsx
@@ -5,23 +5,23 @@ import './logolist.scss'
 export default function LogoList() {
   const partnerLogos = [
     {
-      src: '/images/partner_1.svg', 
+      src: '/images/partner_5.png', 
       alt:'Partner'
     },
     {
-      src: '/images/partner_2.svg', 
+      src: '/images/partner_4.png', 
       alt:'Partner'
     },
     {
-      src: '/images/partner_3.svg', 
+      src: '/images/partner_3.png', 
       alt:'Partner'
     },
     {
-      src: '/images/partner_4.svg', 
+      src: '/images/partner_2.png', 
       alt:'Partner'
     },
     {
-      src: '/images/partner_5.svg', 
+      src: '/images/partner_1.png', 
       alt:'Partner'
     }
   ]
